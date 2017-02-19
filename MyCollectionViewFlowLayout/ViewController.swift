c3e1e9ff95edaad197969bcca94ecc6bcd4f6775
ysignal <ysignal@163.com>
2017-02-19 10:27:23 +0800

    Initial Commit

---
+++ MyCollectionViewFlowLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyCollectionViewFlowLayout
+//
+//  Created by minse on 2017/2/19.
+//  Copyright © 2017年 China. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
